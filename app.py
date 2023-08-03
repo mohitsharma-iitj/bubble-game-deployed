618c4ecf8328eb261b582b1a2b1dde9c9d3bfb42
Mohit Sharma <121308533+mohitsharma-iitj@users.noreply.github.com>
2023-08-04 04:38:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,27 +5,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('form.html')
+    # return render_template('form.html')
+    return render_template('index.html')
 
-@app.route('/predict',methods=['Get','POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    choice = request.form['choice_radio']
+# @app.route('/predict',methods=['Get','POST'])
+# def predict():
+#     '''
+#     For rendering results on HTML GUI
+#     '''
+#     choice = request.form['choice_radio']
     
-    #return render_template('form.html', prediction_text='Value be be $ {}'.format(choice))
+#     #return render_template('form.html', prediction_text='Value be be $ {}'.format(choice))
 
-    if(str(choice) == str('Yes')):
-        return render_template('index.html')
-    elif(choice == 'No'):
-        return render_template('form.html', prediction_text='Press yes')
-    else:
-        return render_template('form.html', prediction_text='Please, press yes.')
+#     if(str(choice) == str('Yes')):
+#         return render_template('index.html')
+#     elif(choice == 'No'):
+#         return render_template('form.html', prediction_text='Press yes')
+#     else:
+#         return render_template('form.html', prediction_text='Please, press yes.')
 
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+    # app.run(debug=True)
 
 #app.run(host='localhost',port=80)
     
